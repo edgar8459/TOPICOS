39b36af9f054a32051a7aca6e6072a5e6c8174eb
edgar8459 <edgar.cortezsegura.723@gmail.com>
2025-02-25 22:51:57 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿
+using NaveEspacial;
+using System.Drawing;
+
+Ventana ventana;
+Nave nave;
+Enemigos enemigo1;
+Enemigos enemigo2;
+Enemigos enemigoBoss;
+bool jugar = true;
+bool BossFinal = false;
+
+void Iniciar()
+{
+    ventana = new Ventana(170, 45, ConsoleColor.Black, new Point(5, 3), new Point(165, 43)); // 213 y 50
+    ventana.DibujaMarco();
+    nave = new Nave(new Point(80,30),ConsoleColor.Red,ventana);
+    nave.Dibujar();
+    enemigo1 = new Enemigos(new Point(50,10),ConsoleColor.Cyan,ventana,TipoEnemigo.Normal,
+        nave);
+    enemigo1.Dibujar();
+    enemigo2 = new Enemigos(new Point(20,12), ConsoleColor.Red, ventana, TipoEnemigo.Normal
+        ,nave);
+    enemigo2.Dibujar();
+    enemigoBoss = new Enemigos(new Point(100, 10), ConsoleColor.Magenta, ventana, TipoEnemigo.Boss,
+        nave);
+
+    nave.Enemigos.Add(enemigo1);
+    nave.Enemigos.Add(enemigo2);
+    nave.Enemigos.Add(enemigoBoss); 
+
+}
+void Game ()
+{
+    while(jugar)
+    {
+        if(!enemigo1.Vivo && !enemigo2.Vivo && !BossFinal)
+        {
+            BossFinal = true;
+            ventana.Peligro();
+        }
+        if(BossFinal)
+        {
+            enemigoBoss.Mover();
+            enemigoBoss.Informacion(140);
+        }
+        else
+        {
+            enemigo1.Mover();
+            enemigo1.Informacion(100);
+            enemigo2.Mover();
+            enemigo2.Informacion(120);
+        }
+
+        nave.Mover(2);
+        nave.Disparar();
+        if(nave.Vida <= 0 )
+        {
+            jugar = false;
+            nave.Muerte();
+        }
+        if(!enemigoBoss.Vivo)
+        {
+            jugar = false;
+        }
+    }
+}
+Iniciar();
+Game();
---
+++ Ventana.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace NaveEspacial
+{
+    internal class Ventana
+    {
+        public int Ancho { get; set; }
+        public int Altura { get; set; }
+        public Point LimiteSuperior { get; set; }
+        public Point LimiteInferior { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public Ventana(int ancho, int altura, ConsoleColor color, Point limiteSupeior, Point limiteInferior)
+        {
+            Ancho = ancho;
+            Altura = altura;
+            Color = color;
+            LimiteSuperior = limiteSupeior;
+            LimiteInferior = limiteInferior;
+
+            Init();
+        }
+        private void Init()
+        {
+            Console.SetWindowSize(Ancho,Altura);
+            Console.Title = "NAVE";
+            Console.CursorVisible = false;
+            Console.BackgroundColor = Color;
+            Console.Clear();
+        }
+        public void DibujaMarco()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            for(int i = LimiteSuperior.X; i<= LimiteInferior.X; i++)
+            {
+                Console.SetCursorPosition(i,LimiteSuperior.Y);
+                Console.Write("═");
+                Console.SetCursorPosition(i, LimiteInferior.Y);
+                Console.Write("═");
+            }
+            for(int i = LimiteSuperior.Y; i <= LimiteInferior.Y; i++)
+            {
+                Console.SetCursorPosition(LimiteSuperior.X,i);
+                Console.Write("║");
+                Console.SetCursorPosition(LimiteInferior.X,i);
+                Console.Write("║");
+            }
+            Console.SetCursorPosition(LimiteSuperior.X,LimiteSuperior.Y);
+            Console.Write("╔");
+            Console.SetCursorPosition(LimiteSuperior.X, LimiteInferior.Y);
+            Console.Write("╚");
+            Console.SetCursorPosition(LimiteInferior.X, LimiteSuperior.Y);
+            Console.Write("╗");
+            Console.SetCursorPosition(LimiteInferior.X, LimiteInferior.Y);
+            Console.Write("╝");
+        }
+        public void Peligro()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(LimiteInferior.X/2-5,LimiteInferior.Y/2);
+            Console.Write("¡¡ PELIGROOO !!");
+            Thread.Sleep(200);
+            Console.SetCursorPosition(LimiteInferior.X/2-5,LimiteInferior.Y/2);
+            Console.Write("               ");
+            Thread.Sleep(200);
+        }
+    }
+}
